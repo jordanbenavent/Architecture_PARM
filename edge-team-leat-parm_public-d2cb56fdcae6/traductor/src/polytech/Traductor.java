ac64b2f9d190e4d2e96a8ef7f6a4e3dc1d588c41
Matthieu <matthieuferaud31@gmail.com>
2021-12-13 15:29:00 +0100

    fix

--- edge-team-leat-parm_public-d2cb56fdcae6/traductor/src/polytech/Traductor.java
+++ edge-team-leat-parm_public-d2cb56fdcae6/traductor/src/polytech/Traductor.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Traductor {
@@ -14,6 +15,7 @@ public class Traductor {
     private final Path path;
     private final List<Instruction> instructions;
     private List<String> lines;
+    private HashMap<String, List<String>> fonctions;
 
     Traductor(String path) {
         this.path = Paths.get(path);
@@ -21,6 +23,19 @@ public class Traductor {
     }
 
     void read() throws IOException {
+        lines = Files.readAllLines(path);
+        String name = null;
+        for (String line : lines) {
+            if (line.startsWith("run")) {
+                name = "run";
+                fonctions.put(name, new ArrayList<>());
+            } else if (line.startsWith(".LBB0_")) {
+                name = line.substring(0, line.indexOf(":"));
+                fonctions.put(name, new ArrayList<>());
+            } else if(name != null){
+                fonctions.get(name).add(line);
+            }
+        }
         lines = Files.readAllLines(path).stream().map(String::trim).filter(l -> l.length() > 0 && l.charAt(0) != '.').toList();
     }
 
@@ -36,6 +51,9 @@ public class Traductor {
                 instructions.add(new Ldr(line));
             } else if (line.startsWith("adds")) {
                 instructions.add(new Adds(line));
+            } else if (line.startsWith("b")) {
+                zsaqzsefbujkoi
+
             }
         }
     }
