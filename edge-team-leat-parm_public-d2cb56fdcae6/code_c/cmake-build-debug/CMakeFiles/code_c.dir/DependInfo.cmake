f530ea87e39f2db01174bb48af095e9ed7aa210c
Mattheex <matthieuferaud31@gmail.com>
2022-01-08 19:11:34 +0100

    code c main

---
+++ edge-team-leat-parm_public-d2cb56fdcae6/code_c/cmake-build-debug/CMakeFiles/code_c.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/matthieu/Documents/Architecture_PARM/edge-team-leat-parm_public-d2cb56fdcae6/code_c/main.c" "/home/matthieu/Documents/Architecture_PARM/edge-team-leat-parm_public-d2cb56fdcae6/code_c/cmake-build-debug/CMakeFiles/code_c.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  "../include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
